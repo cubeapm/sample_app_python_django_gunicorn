a289d35727839021cac2465d7ec7a03ffa38b696
Ishaan Parashar <89313489+ishaan766@users.noreply.github.com>
2023-10-26 09:16:21 +0530

    Update settings.py default dbsqlite database

--- sample_app/settings.py
+++ sample_app/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'sample_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sql',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
