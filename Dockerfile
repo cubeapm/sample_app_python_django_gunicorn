b6d1f1899071f35f98d09544ff9e94c4bd805145
ishaan766 <ishaan766@gmail.com>
2025-02-08 12:39:42 +0530

    Python Django Gunicorn Instrumentation

--- Dockerfile
+++ Dockerfile
@@ -11,8 +11,6 @@ ADD . .
 
 RUN pip install -r requirements.txt
 
-RUN opentelemetry-bootstrap -a install
-
 EXPOSE 8000
 
 CMD ["gunicorn", "sample_app.wsgi", "-c", "gunicorn.conf.py"]
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,12 +1,4 @@
 import os
-from opentelemetry import trace
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    BatchSpanProcessor,
-    ConsoleSpanExporter,
-    SimpleSpanProcessor,
-)
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 
 bind = "0.0.0.0:8000"
 
@@ -24,15 +16,3 @@ accesslog = "-"
 access_log_format = (
     '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 )
-
-
-def post_fork(server, worker):
-    server.log.info("Worker spawned (pid: %s)", worker.pid)
-
-    provider = TracerProvider()
-    if os.getenv('OTEL_LOG_LEVEL', '') == 'debug':
-        processor = SimpleSpanProcessor(ConsoleSpanExporter())
-    else:
-        processor = BatchSpanProcessor(OTLPSpanExporter())
-    provider.add_span_processor(processor)
-    trace.set_tracer_provider(provider)
